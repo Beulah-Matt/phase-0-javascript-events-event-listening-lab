5b90b25770ad495297cba389715427fb2fd19919
Beulah Mathenge <beulahmathenge@gmail.com>
2022-08-05 23:38:42 +0300

    Completed Assignment

--- index.js
+++ index.js
@@ -1,5 +1,10 @@
+const button= document.getElementById('button')
 
 function addingEventListener() {
-    
-  
+   button.addEventListener('click',clickAlert) 
 }
+function clickAlert(){
+    
+alert('I was clicked');  
+}
+  
